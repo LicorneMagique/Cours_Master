bbccbde182b72291c775d68c6b6c6ccdb4a37196
Julien Giraud <giraud740@gmail.com>
2021-01-10 00:28:53 +0100

    Ajout corrections IA

--- Bases_de_l'intelligence_artificielle/TP/tp4.pl
+++ Bases_de_l'intelligence_artificielle/TP/tp4.pl
@@ -5,8 +5,9 @@ somme(A, B, C) :-
     X is A + B + C,
     X = 15.
 
-unique(X, [L]) :-
-    not member(X, L).
+unique([T|R]) :-
+    not(member(T, R)),
+    unique(R).
 
 carre([A, B, C, D, E, F, G, H, I]) :-
     nombre(A),
@@ -18,7 +19,7 @@ carre([A, B, C, D, E, F, G, H, I]) :-
     nombre(G),
     nombre(H),
     nombre(I),
-    unique(A,B,C,D,E,F,G,H,I),
+    unique([A,B,C,D,E,F,G,H,I]),
     somme(A, B, C),
     somme(D, E, F),
     somme(G, H, I),
